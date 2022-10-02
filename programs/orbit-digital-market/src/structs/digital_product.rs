9c623dbaddd581b5f986adb4ac89131801564be2
indianeros58 <0xDEADBEAT@gmail.com>
2022-10-02 04:49:11 -0400

    comment

--- programs/orbit-digital-market/src/structs/digital_product.rs
+++ programs/orbit-digital-market/src/structs/digital_product.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
 use product::product_struct::OrbitProduct;
 
+// disc b910f3a2 [185, 16, 243, 162, 235, 96, 85, 214]
 #[account]
 pub struct DigitalProduct{
     pub metadata: OrbitProduct,
