4179b46649a01ca58af8b43b9b54e348c4a93ff0
0xDEADBEAT <indianerios58@gmail.com>
2023-03-23 10:41:55 -0400

    ?

--- tests/orbit-digital-market.ts
+++ tests/orbit-digital-market.ts
@@ -1,5 +1,5 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
 import { OrbitDigitalMarket } from "../target/types/orbit_digital_market";
 
 describe("orbit-digital-market", () => {
